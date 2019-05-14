2b4e81bc1d4a33b84ccc707bb59d610a82258039
Tommy <englerttw@mymail.vcu.edu>
2019-05-13 20:59:16 -0400

    adding initial files

---
+++ 1-flask/program/routes.py
@@ -0,0 +1,16 @@
+from program import app
+from flask import render_template
+
+@app.route('/')
+@app.route('/index')
+
+def index():
+    return render_template('index.html')
+
+@app.route('/100days')
+def p100days():
+    return render_template('100days.html')
+
+@app.route('/practice')
+def practice():
+    return render_template('practice.html')
